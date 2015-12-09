e815e3a4e0a93a1c5b7ea3cb2863c62df8ebf576
Ryan Printup <ryan.printup@gmail.com>
2015-12-08 19:15:16 -0500

    Merge listener into client class

--- src/com/ryanprintup/soar/server/ChatClient.java
+++ src/com/ryanprintup/soar/server/ChatClient.java
@@ -1,18 +1,24 @@
 package com.ryanprintup.soar.server;
 
+import com.ryanprintup.soar.Soar;
 import com.ryanprintup.soar.utils.Debug;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class ChatClient
+public class ChatClient implements Runnable
 {
     private Socket socket;
     private DataInputStream inStream;
     private DataOutputStream outStream;
-    
-    private ChatClientListener listenerThread;
+
+    private String name;
+    private long id;
+
+    private boolean running;
+    private Thread recieveThread;
     
     public ChatClient(Socket socket)
     {
@@ -24,13 +30,45 @@ public class ChatClient
         } catch (IOException e) {
             if (Debug.ERRORS) Debug.log("Failed to get input and output stream: " + e);
         }
-        
-        listenerThread = new ChatClientListener(this, inStream);
-        listenerThread.start();
+
+        recieveThread = new Thread(this);
     }
-    
+
+    public byte[] recieve() throws IOException
+    {
+        byte[] buffer = new byte[2014];
+        inStream.read(buffer);
+
+        return buffer;
+    }
+
     public void send(byte[] packet) throws IOException
     {
         outStream.write(packet);
+    }
+
+    public void startRecieveLoop()
+    {
+        running = true;
+        recieveThread.start();
+    }
+
+    @Override
+    public void run()
+    {
+        while (running) {
+            try {
+                byte[] packet = recieve();
+                Soar.getServer().handleIncomingPacket(this, packet);
+            } catch (IOException e) {
+                if (Debug.ERRORS) Debug.log("Failed to recieve message from [" + id + "] " + name + ": " + e);
+            }
+        }
+    }
+
+    public void stopRecieveLoop()
+    {
+        running = false;
+        recieveThread.interrupt();
     }
 }
