952a958cfe2d1a6565f07c9de68c6eb8e7ba06b7
Ryan Printup <ryan.printup@gmail.com>
2015-12-08 19:06:20 -0500

    Format (It was annoying me)

--- src/com/ryanprintup/soar/net/PacketTypes.java
+++ src/com/ryanprintup/soar/net/PacketTypes.java
@@ -2,7 +2,7 @@ package com.ryanprintup.soar.net;
 
 public final class PacketTypes
 {
-    public static final byte HEARTBEAT_PACKET            = 0x00;
+    public static final byte HEARTBEAT_PACKET           = 0x00;
     public static final byte CLIENT_CONNCET_PACKET      = 0x01;
     public static final byte CONNECTION_RESPONSE_PACKET = 0x02;
     public static final byte CHAT_MESSAGE_PACKET        = 0x03;
